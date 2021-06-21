7e3a818dcbbac82a6e92ebe7a2cad2d7a6b70924
jagilley <37783831+jagilley@users.noreply.github.com>
2021-06-20 23:33:53 -0500

    roman numberal parser working

--- pychord/utils.py
+++ pychord/utils.py
@@ -1,10 +1,34 @@
 # -*- coding: utf-8 -*-
-
-from .constants import NOTE_VAL_DICT, SCALE_VAL_DICT
+if __name__!="__main__":
+    from .constants import NOTE_VAL_DICT, SCALE_VAL_DICT
 
 def roman_numeral_parser(string):
-    pass
-
+    def romanToInt(s):
+      roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+      # TODO: add this dict to the constants files
+      i = 0
+      num = 0
+      while i < len(s):
+         if i+1<len(s) and s[i:i+2] in roman:
+            num+=roman[s[i:i+2]]
+            i+=2
+         else:
+            #print(i)
+            num+=roman[s[i]]
+            i+=1
+      return num
+    numbrz = [str(i) for i in list(range(0,10))]
+    chords = string.split("-")
+    ret_list = []
+    for chord in chords:
+        prs_chord = chord
+        quality = ""
+        for numbr in numbrz:
+            if numbr in prs_chord:
+                prs_chord = prs_chord.replace(numbr, "")
+                quality = numbr
+        ret_list.append((romanToInt(prs_chord), quality))
+    return ret_list
 
 def note_to_val(note):
     """ Convert note to int
@@ -61,3 +85,6 @@ def display_on(on_note):
     if on_note:
         return "/{}".format(on_note)
     return ""
+
+if __name__=="__main__":
+    print(roman_numeral_parser("I-IV-V7-I"))
